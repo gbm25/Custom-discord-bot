3e952512ab0f59722f4b92e0bc438ee6991dbf15
Gbm25 <gonzalo.busto@gmail.com>
2021-08-17 18:14:26 +0200

    Se añaden las funciones genshin_banners_to_json() y json_to_genshin_banners() para serializar y deserializar el objeto GenshinBanner

--- data_management.py
+++ data_management.py
@@ -1,8 +1,12 @@
 import json
 import os
+from datetime import datetime
 
 from GI_code import GenshinCode
 from GI_reward import GenshinReward
+from GI_banner import GenshinBanner
+from GI_servertime import GenshinImpactServerTime
+
 
 
 def serialization_json(relative_path, file, data):
@@ -11,7 +15,7 @@ def serialization_json(relative_path, file, data):
         # Open file to write
         with open(f'{relative_path}{file}.json', 'w+') as write:
             json.dump(data, write, indent=4)
-        write.close()
+
 
 
 def deserialization_json(relative_path, file):
@@ -44,7 +48,7 @@ def genshin_codes_to_json(relative_path, file, data):
             # Open file to write
             with open(f'{relative_path}{file}.json', 'w+') as write:
                 json.dump(temp_dict, write, indent=4)
-            write.close()
+
     except json.JSONDecodeError:
 
         return {}
@@ -75,6 +79,59 @@ def json_to_genshin_codes(relative_path, file):
                 ))
 
             data['codes'] = codes
+
+        return data
+
+    except json.JSONDecodeError:
+
+        return {}
+
+
+def genshin_banners_to_json(relative_path, file, data):
+    try:
+        # if no empty parameter
+        if relative_path and file and data['banners']:
+            temp_dict = {'banners': [banner.asdict() for banner in data['banners']]}
+
+            # Open file to write
+            with open(f'{relative_path}{file}.json', 'w+') as write:
+                json.dump(temp_dict, write, indent=4)
+
+    except json.JSONDecodeError:
+
+        return {}
+
+
+def json_to_genshin_banners(relative_path, file):
+    try:
+        data = {}
+        # if no empty parameter adn file exists
+        if relative_path and file and os.path.exists(f'{relative_path}{file}.json'):
+            # Open JSON file
+            json_file = open(f'{relative_path}{file}.json', )
+            # JSON object as a dictionary
+            data = json.load(json_file)
+
+            # Closing the JSON file
+            json_file.close()
+            banners = []
+
+            for banner in data['banners']:
+
+                banners.append(GenshinBanner(
+                    name=banner['name'],
+                    url_fandom=banner['url_fandom'],
+                    url_official=banner['url_official'],
+                    image=banner['image'],
+                    status=banner['status'],
+
+                    start=[GenshinImpactServerTime(gi_datetime["region"],datetime.fromisoformat(gi_datetime["datetime"]))
+                           for gi_datetime in banner['start']],
+                    end=[GenshinImpactServerTime(gi_datetime["region"], datetime.fromisoformat(gi_datetime["datetime"]))
+                           for gi_datetime in banner['end']]
+                ))
+
+            data['banners'] = banners
 
         return data
 
